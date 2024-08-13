028d417fcb1439188ff6701b2ba95d609b4e9d45
Carlos Febres <febres0492@gmail.com>
2024-08-12 20:32:47 -0400

    refactor: Improve structure and styling of AddProduct component

--- client/src/components/AddProduct/index.jsx
+++ client/src/components/AddProduct/index.jsx
@@ -42,37 +42,39 @@ const AddProduct = () => {
     };
 
     return (
-        <div className="container my-1">
-            <div className='card my-4 p-3'>
-                <h1>Add Product</h1>
-                <form className='d-flex flex-column gap-2' onSubmit={handleSubmit} >
-                    <div className="border p-2 d-flex flex-column">
-                        <input type="text" name="name" placeholder="Name" value={product.name} onChange={handleChange} required />
-                        <textarea name="description" placeholder="Description" value={product.description} onChange={handleChange} required rows="4" />
-                    </div>
-                    <div className="df ais gap-2">
-                        <input className='border fg' type="number" name="price" placeholder="Price" value={product.price} onChange={handleChange} required />
-                        <input className='border fg' type="number" name="quantity" placeholder="Quantity" value={product.quantity} onChange={handleChange} required />
-                        <select className='border fg' name="category" value={product.category} onChange={handleChange} required>
-                            <option value="">Select Category</option>
-                            {categories.map((category) => (
-                                <option key={category._id} value={category._id}>
-                                    {category.name}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    <input className='border' type="text" name="zipcode" placeholder="Zipcode" value={product.zipcode} onChange={handleChange} required />
-                    <div className='df aic gap-2 p-2 border'>
-                        <label>Product Image:</label>
-                        <input type="file" name="image" onChange={handleFileChange} />
-                    </div>
-                    <button className="btn-1 bg-c1" type="submit" disabled={loading}>
-                        {loading ? 'Adding...' : 'Add Product'}
-                    </button>
-                    {error && <p>Error: {error.message}</p>}
-                </form>
-                {data && <p>Product added successfully!</p>}
+        <div className="container df my-1 jcc">
+            <div className='rounded bg-d1 fg df jcc p-3'>
+                <div className='card my-4 p-3' style={{maxWidth:'600px'}}>
+                    <h1>Add Product</h1>
+                    <form className='d-flex flex-column gap-2' onSubmit={handleSubmit} >
+                        <div className="border p-2 d-flex flex-column">
+                            <input type="text" name="name" placeholder="Name" value={product.name} onChange={handleChange} required />
+                            <textarea name="description" placeholder="Description" value={product.description} onChange={handleChange} required rows="4" />
+                        </div>
+                        <div className="df ais gap-2">
+                            <input className='border fg' type="number" name="price" placeholder="Price" value={product.price} onChange={handleChange} required />
+                            <input className='border fg' type="number" name="quantity" placeholder="Quantity" value={product.quantity} onChange={handleChange} required />
+                            <select className='border fg' name="category" value={product.category} onChange={handleChange} required>
+                                <option value="">Select Category</option>
+                                {categories.map((category) => (
+                                    <option key={category._id} value={category._id}>
+                                        {category.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        <input className='border' type="text" name="zipcode" placeholder="Zipcode" value={product.zipcode} onChange={handleChange} required />
+                        <div className='df aic gap-2 p-2 border'>
+                            <label>Product Image:</label>
+                            <input type="file" name="image" onChange={handleFileChange} />
+                        </div>
+                        <button className="btn-1 bg-c1" type="submit" disabled={loading}>
+                            {loading ? 'Adding...' : 'Add Product'}
+                        </button>
+                        {error && <p>Error: {error.message}</p>}
+                    </form>
+                    {data && <p>Product added successfully!</p>}
+                </div>
             </div>
         </div>
     );
